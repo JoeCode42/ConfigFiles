40f8c31e41ab14adc96045012a437d2754a18068
Alexander Heinisch <e11906270@student.tuwien.ac.at>
2022-09-12 08:29:17 +0200

    added comments better describing what to change in peco_change_directory

--- .config/fish/functions/peco_change_directory.fish
+++ .config/fish/functions/peco_change_directory.fish
@@ -1,28 +1,30 @@
 function _peco_change_directory
-  if [ (count $argv) ]
-    peco --layout=bottom-up --query "$argv " | perl -pe 's/([ ()])/\\\\$1/g' | sed "s/[\]//g" | read tmp
-  else 
-    peco --layout=bottom-up | perl -pe 's/([ ()])/\\\\$1/g'| sed "s/[\]//g" |read tmp
-  end
+    if [ (count $argv) ]
+        peco --layout=bottom-up --query "$argv " | perl -pe 's/([ ()])/\\\\$1/g' | sed "s/[\]//g" | read tmp
+    else 
+        peco --layout=bottom-up | perl -pe 's/([ ()])/\\\\$1/g'| sed "s/[\]//g" |read tmp
+    end
 
-  if [ $tmp ]
-    builtin cd $tmp
-    commandline -r ''
-    commandline -f repaint
-  else
-    commandline ''
-  end
+    if [ $tmp ]
+        builtin cd $tmp
+        commandline -r ''
+        commandline -f repaint
+    else
+        commandline ''
+    end
 end
 
 function peco_change_directory
-  # set all the directories to be included in the search here
-  set currents */
+    # set all the directories to be included in the search here
+    set currents */
 
-  begin
-    echo $HOME/.config
-    if count $currents >/dev/null
-        ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
-    end
-    ls -ad $HOME/Nextcloud/Projects/*/ | grep -v \.git
-  end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
+    begin
+        echo $HOME/.config
+        if count $currents >/dev/null
+            ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
+        end
+
+        # add your own custom paths here like this
+        ls -ad /media/alex/Data/Nextcloud/Projects/*/ | grep -v \.git
+    end | sed -e 's/\/$//' | awk '!a[$0]++' | _peco_change_directory $argv
 end
