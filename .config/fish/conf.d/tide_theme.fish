cb72b63ec7994e0103769ce8ff21179b60297641
AlexHeinisch <6390741+AlexHeinisch@users.noreply.github.com>
2022-09-20 21:17:52 +0200

    modified color scheme for os icon

--- .config/fish/conf.d/tide_theme.fish
+++ .config/fish/conf.d/tide_theme.fish
@@ -1,7 +1,10 @@
-set -u tide_pwd_bg_color 007070
+set -u tide_pwd_bg_color 0C3645
 set -u tide_pwd_color_anchors E4E4E4
 set -u tide_pwd_color_dirs white 
 set -u tide_pwd_color_truncated_dirs white 
 
 set -u fish_color_param 029999
 set -u fish_color_command white 
+
+set -u tide_os_bg_color 268BD2
+set -u tide_os_color black
